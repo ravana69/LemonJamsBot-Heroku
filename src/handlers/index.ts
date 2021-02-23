5eff87f5a67a26b58d273ea5b36bf1f46d3f71a1
Ravana <47528708+ravana69@users.noreply.github.com>
2021-02-23 10:38:03 +0530

    Update index.ts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,12 +2,14 @@ import { bot } from '../bot';
 
 import { playHandler } from './baja';
 import { queueHandler } from './dikha';
+import { songHandler } from './song';
 import { pauseHandler } from './ruk';
 import { skipHandler } from './nikal';
 
 export const initHandlers = (): void => {
     bot.use(playHandler);
     bot.use(queueHandler);
+    bot.use(songHandler);
     bot.use(pauseHandler);
     bot.use(skipHandler);
 };
