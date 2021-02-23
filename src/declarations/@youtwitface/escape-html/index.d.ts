ca477e4dc605fb19c87ee7d293bc5ff506368a06
Ravana <47528708+ravana69@users.noreply.github.com>
2021-02-23 10:36:50 +0530

    Update index.d.ts

--- src/declarations/@youtwitface/escape-html/index.d.ts
+++ src/declarations/@youtwitface/escape-html/index.d.ts
@@ -1,15 +1,4 @@
-import { bot } from '../bot';
-
-import { playHandler } from './baja';
-import { queueHandler } from './dikha';
-import { songHandler } from './song';
-import { pauseHandler } from './ruk';
-import { skipHandler } from './nikal';
-
-export const initHandlers = (): void => {
-    bot.use(playHandler);
-    bot.use(queueHandler);
-    bot.use(songHandler);
-    bot.use(pauseHandler);
-    bot.use(skipHandler);
-};
+declare module '@youtwitface/escape-html' {
+    const escapeHtml: (html: string) => string;
+    export default escapeHtml;
+}
